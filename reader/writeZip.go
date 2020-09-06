651765e4aaaf5cd690c6b6f1de38bc1798a74165
shota-tsuji <shotatuzi@gmail.com>
2020-09-06 09:35:09 +0900

    feat: 重複の削除

--- reader/writeZip.go
+++ reader/writeZip.go
@@ -20,7 +20,6 @@ func main() {
 	filenames := [...] string {"file1-for-zip.txt", "file2-for-zip.txt"}
 	for _, fileName := range filenames {
 		fmt.Println(fileName)
-		//fileName := "file1-for-zip.txt"
 		file, err := os.Open(fileName)
 		if err != nil {
 			panic(err)
@@ -30,23 +29,4 @@ func main() {
 		writer, err := zipWriter.Create(fileName)
 		io.Copy(writer, file)
 	}
-	//fileName := "file1-for-zip.txt"
-	//file, err := os.Open(fileName)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer file.Close()
-
-	//writer, err := zipWriter.Create(fileName)
-	//io.Copy(writer, file)
-
-	//fileName := "file2-for-zip.txt"
-	//file, err := os.Open(fileName)
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer file.Close()
-
-	//writer, err := zipWriter.Create(fileName)
-	//io.Copy(writer, file)
 }
